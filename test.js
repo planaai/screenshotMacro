f6557935125533517bd1cad8da8da7a1fe79ae4a
planaai <dev@planaai.com>
2026-07-19 00:43:37 +0900

    Clean up unnecessary test files and output folders

--- test.js
+++
@@ -1,7 +0,0 @@
-﻿const { prisma } = require('./db.js');
-async function run() {
-  console.log('Collection:', await prisma.collection.findFirst({where: {studentId: 187}}));
-  console.log('GrowthPlan:', await prisma.growthPlan.findFirst({where: {studentId: 187}}));
-  console.log('StudentStats:', await prisma.studentStats.findFirst({where: {studentId: 187}}));
-}
-run().finally(() => prisma.$disconnect());
